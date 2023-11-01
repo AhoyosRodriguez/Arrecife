c736bd483843a60e5032d84beb5b6b99621be332
AhoyosRodriguez <122849304+AhoyosRodriguez@users.noreply.github.com>
2023-11-01 14:31:34 -0500

    Add files via upload

---
+++ Icetex 2023/login.js
@@ -0,0 +1,19 @@
+function login()
+{
+
+var user= document.getElementById("usuario").value;
+
+var pass= document.getElementById("pass").value;
+
+
+if(user=="juan" && pass=="1234")
+
+{
+
+
+window.location="Bienvenida.html";
+
+}
+
+
+}
---
+++ Icetex 2023/swimsuits.js
@@ -0,0 +1,33 @@
+/* Esta lista podría ser reemplazada por la respuesta de un backend */
+const swimsuits = [
+    {
+      id:1,
+      nombre: " escote cuadrado",
+      precio: 90.000,
+    },
+    {
+      id:2,
+      nombre: "escote en espalda",
+      precio: 120.000,
+    },
+    {
+      id:3,
+      nombre: "escote frontal",
+      precio: 160.000,
+    },
+    {
+      id:4,
+      nombre: "octupus",
+      precio: 200.000,
+    },
+    {
+      id:5,
+      nombre: "tiburones ilustracion",
+      precio: 90.000,
+    },
+    {
+      id:6,
+      nombre: "triangulos",
+      precio: 160.000,
+    }
+  ]
